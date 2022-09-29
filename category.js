360eb6d4dd9af36c52ed58559b42bc3a1eefecbd
Lateraluz <43474323+lateraluz@users.noreply.github.com>
2022-09-28 20:14:00 -0600

    Create category.js

---
+++ category.js
@@ -0,0 +1,42 @@
+[[
+	{
+		"IdCategory": 1,
+		"CategoryName": "Beverages",
+		"Description": "Soft drinks, coffees, teas, beers, and ales"
+	},
+	{
+		"IdCategory": 2,
+		"CategoryName": "Condiments",
+		"Description": "Sweet and savory sauces, relishes, spreads, and seasonings"
+	},
+	{
+		"IdCategory": 3,
+		"CategoryName": "Confections",
+		"Description": "Desserts, candies, and sweet breads"
+	},
+	{
+		"IdCategory": 4,
+		"CategoryName": "Dairy Products",
+		"Description": "Cheeses"
+	},
+	{
+		"IdCategory": 5,
+		"CategoryName": "Grains/Cereals",
+		"Description": "Breads, crackers, pasta, and cereal"
+	},
+	{
+		"IdCategory": 6,
+		"CategoryName": "Meat/Poultry",
+		"Description": "Prepared meats"
+	},
+	{
+		"IdCategory": 7,
+		"CategoryName": "Produce",
+		"Description": "Dried fruit and bean curd"
+	},
+	{
+		"IdCategory": 8,
+		"CategoryName": "Seafood",
+		"Description": "Seaweed and fish"
+	}
+]]
