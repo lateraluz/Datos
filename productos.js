a94262c98b438a196a9d30e1e2c90a9f42fa3546
Lateraluz <43474323+lateraluz@users.noreply.github.com>
2022-09-28 20:07:10 -0600

    Create productos.js

---
+++ productos.js
@@ -0,0 +1,772 @@
+[{
+		"IdProduct": 1,
+		"ProductName": "Chai",
+		"IdWarehouse": 1,
+		"UnitPrice": 18.0,
+		"UnitsInStock": 39,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 10,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 2,
+		"ProductName": "Chang",
+		"IdWarehouse": 1,
+		"UnitPrice": 19.0,
+		"UnitsInStock": 17,
+		"UnitsOnOrder": 40,
+		"ReorderLevel": 25,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 3,
+		"ProductName": "Aniseed Syrup",
+		"IdWarehouse": 2,
+		"UnitPrice": 10.0,
+		"UnitsInStock": 13,
+		"UnitsOnOrder": 70,
+		"ReorderLevel": 25,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 4,
+		"ProductName": "Chef Anton's Cajun Seasoning",
+		"IdWarehouse": 2,
+		"UnitPrice": 22.0,
+		"UnitsInStock": 53,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 5,
+		"ProductName": "Chef Anton's Gumbo Mix",
+		"IdWarehouse": 2,
+		"UnitPrice": 21.35,
+		"UnitsInStock": 0,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": true
+	},
+	{
+		"IdProduct": 6,
+		"ProductName": "Grandma's Boysenberry Spread",
+		"IdWarehouse": 2,
+		"UnitPrice": 25.0,
+		"UnitsInStock": 120,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 25,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 7,
+		"ProductName": "Uncle Bob's Organic Dried Pears",
+		"IdWarehouse": 7,
+		"UnitPrice": 30.0,
+		"UnitsInStock": 15,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 10,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 8,
+		"ProductName": "Northwoods Cranberry Sauce",
+		"IdWarehouse": 2,
+		"UnitPrice": 40.0,
+		"UnitsInStock": 6,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 9,
+		"ProductName": "Mishi Kobe Niku",
+		"IdWarehouse": 6,
+		"UnitPrice": 97.0,
+		"UnitsInStock": 29,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": true
+	},
+	{
+		"IdProduct": 10,
+		"ProductName": "Ikura",
+		"IdWarehouse": 8,
+		"UnitPrice": 31.0,
+		"UnitsInStock": 31,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 11,
+		"ProductName": "Queso Cabrales",
+		"IdWarehouse": 4,
+		"UnitPrice": 21.0,
+		"UnitsInStock": 22,
+		"UnitsOnOrder": 30,
+		"ReorderLevel": 30,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 12,
+		"ProductName": "Queso Manchego La Pastora",
+		"IdWarehouse": 4,
+		"UnitPrice": 38.0,
+		"UnitsInStock": 86,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 13,
+		"ProductName": "Konbu",
+		"IdWarehouse": 8,
+		"UnitPrice": 6.0,
+		"UnitsInStock": 24,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 5,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 14,
+		"ProductName": "Tofu",
+		"IdWarehouse": 7,
+		"UnitPrice": 23.25,
+		"UnitsInStock": 35,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 15,
+		"ProductName": "Genen Shouyu",
+		"IdWarehouse": 2,
+		"UnitPrice": 15.5,
+		"UnitsInStock": 39,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 5,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 16,
+		"ProductName": "Pavlova",
+		"IdWarehouse": 3,
+		"UnitPrice": 17.45,
+		"UnitsInStock": 29,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 10,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 17,
+		"ProductName": "Alice Mutton",
+		"IdWarehouse": 6,
+		"UnitPrice": 39.0,
+		"UnitsInStock": 0,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": true
+	},
+	{
+		"IdProduct": 18,
+		"ProductName": "Carnarvon Tigers",
+		"IdWarehouse": 8,
+		"UnitPrice": 62.5,
+		"UnitsInStock": 42,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 19,
+		"ProductName": "Teatime Chocolate Biscuits",
+		"IdWarehouse": 3,
+		"UnitPrice": 9.2,
+		"UnitsInStock": 25,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 5,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 20,
+		"ProductName": "Sir Rodney's Marmalade",
+		"IdWarehouse": 3,
+		"UnitPrice": 81.0,
+		"UnitsInStock": 40,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 21,
+		"ProductName": "Sir Rodney's Scones",
+		"IdWarehouse": 3,
+		"UnitPrice": 10.0,
+		"UnitsInStock": 3,
+		"UnitsOnOrder": 40,
+		"ReorderLevel": 5,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 22,
+		"ProductName": "Gustaf's Knäckebröd",
+		"IdWarehouse": 5,
+		"UnitPrice": 21.0,
+		"UnitsInStock": 104,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 25,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 23,
+		"ProductName": "Tunnbröd",
+		"IdWarehouse": 5,
+		"UnitPrice": 9.0,
+		"UnitsInStock": 61,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 25,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 24,
+		"ProductName": "Guaraná Fantástica",
+		"IdWarehouse": 1,
+		"UnitPrice": 4.5,
+		"UnitsInStock": 20,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": true
+	},
+	{
+		"IdProduct": 25,
+		"ProductName": "NuNuCa Nuß-Nougat-Creme",
+		"IdWarehouse": 3,
+		"UnitPrice": 14.0,
+		"UnitsInStock": 76,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 30,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 26,
+		"ProductName": "Gumbär Gummibärchen",
+		"IdWarehouse": 3,
+		"UnitPrice": 31.23,
+		"UnitsInStock": 15,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 27,
+		"ProductName": "Schoggi Schokolade",
+		"IdWarehouse": 3,
+		"UnitPrice": 43.9,
+		"UnitsInStock": 49,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 30,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 28,
+		"ProductName": "Rössle Sauerkraut",
+		"IdWarehouse": 7,
+		"UnitPrice": 45.6,
+		"UnitsInStock": 26,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": true
+	},
+	{
+		"IdProduct": 29,
+		"ProductName": "Thüringer Rostbratwurst",
+		"IdWarehouse": 6,
+		"UnitPrice": 123.79,
+		"UnitsInStock": 0,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": true
+	},
+	{
+		"IdProduct": 30,
+		"ProductName": "Nord-Ost Matjeshering",
+		"IdWarehouse": 8,
+		"UnitPrice": 25.89,
+		"UnitsInStock": 10,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 15,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 31,
+		"ProductName": "Gorgonzola Telino",
+		"IdWarehouse": 4,
+		"UnitPrice": 12.5,
+		"UnitsInStock": 0,
+		"UnitsOnOrder": 70,
+		"ReorderLevel": 20,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 32,
+		"ProductName": "Mascarpone Fabioli",
+		"IdWarehouse": 4,
+		"UnitPrice": 32.0,
+		"UnitsInStock": 9,
+		"UnitsOnOrder": 40,
+		"ReorderLevel": 25,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 33,
+		"ProductName": "Geitost",
+		"IdWarehouse": 4,
+		"UnitPrice": 2.5,
+		"UnitsInStock": 112,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 20,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 34,
+		"ProductName": "Sasquatch Ale",
+		"IdWarehouse": 1,
+		"UnitPrice": 14.0,
+		"UnitsInStock": 111,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 15,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 35,
+		"ProductName": "Steeleye Stout",
+		"IdWarehouse": 1,
+		"UnitPrice": 18.0,
+		"UnitsInStock": 20,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 15,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 36,
+		"ProductName": "Inlagd Sill",
+		"IdWarehouse": 8,
+		"UnitPrice": 19.0,
+		"UnitsInStock": 112,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 20,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 37,
+		"ProductName": "Gravad lax",
+		"IdWarehouse": 8,
+		"UnitPrice": 26.0,
+		"UnitsInStock": 11,
+		"UnitsOnOrder": 50,
+		"ReorderLevel": 25,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 38,
+		"ProductName": "Côte de Blaye",
+		"IdWarehouse": 1,
+		"UnitPrice": 263.5,
+		"UnitsInStock": 17,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 15,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 39,
+		"ProductName": "Chartreuse verte",
+		"IdWarehouse": 1,
+		"UnitPrice": 18.0,
+		"UnitsInStock": 69,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 5,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 40,
+		"ProductName": "Boston Crab Meat",
+		"IdWarehouse": 8,
+		"UnitPrice": 18.4,
+		"UnitsInStock": 123,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 30,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 41,
+		"ProductName": "Jack's New England Clam Chowder",
+		"IdWarehouse": 8,
+		"UnitPrice": 9.65,
+		"UnitsInStock": 85,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 10,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 42,
+		"ProductName": "Singaporean Hokkien Fried Mee",
+		"IdWarehouse": 5,
+		"UnitPrice": 14.0,
+		"UnitsInStock": 26,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": true
+	},
+	{
+		"IdProduct": 43,
+		"ProductName": "Ipoh Coffee",
+		"IdWarehouse": 1,
+		"UnitPrice": 46.0,
+		"UnitsInStock": 17,
+		"UnitsOnOrder": 10,
+		"ReorderLevel": 25,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 44,
+		"ProductName": "Gula Malacca",
+		"IdWarehouse": 2,
+		"UnitPrice": 19.45,
+		"UnitsInStock": 27,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 15,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 45,
+		"ProductName": "Rogede sild",
+		"IdWarehouse": 8,
+		"UnitPrice": 9.5,
+		"UnitsInStock": 5,
+		"UnitsOnOrder": 70,
+		"ReorderLevel": 15,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 46,
+		"ProductName": "Spegesild",
+		"IdWarehouse": 8,
+		"UnitPrice": 12.0,
+		"UnitsInStock": 95,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 47,
+		"ProductName": "Zaanse koeken",
+		"IdWarehouse": 3,
+		"UnitPrice": 9.5,
+		"UnitsInStock": 36,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 48,
+		"ProductName": "Chocolade",
+		"IdWarehouse": 3,
+		"UnitPrice": 12.75,
+		"UnitsInStock": 15,
+		"UnitsOnOrder": 70,
+		"ReorderLevel": 25,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 49,
+		"ProductName": "Maxilaku",
+		"IdWarehouse": 3,
+		"UnitPrice": 20.0,
+		"UnitsInStock": 10,
+		"UnitsOnOrder": 60,
+		"ReorderLevel": 15,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 50,
+		"ProductName": "Valkoinen suklaa",
+		"IdWarehouse": 3,
+		"UnitPrice": 16.25,
+		"UnitsInStock": 65,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 30,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 51,
+		"ProductName": "Manjimup Dried Apples",
+		"IdWarehouse": 7,
+		"UnitPrice": 53.0,
+		"UnitsInStock": 20,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 10,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 52,
+		"ProductName": "Filo Mix",
+		"IdWarehouse": 5,
+		"UnitPrice": 7.0,
+		"UnitsInStock": 38,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 25,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 53,
+		"ProductName": "Perth Pasties",
+		"IdWarehouse": 6,
+		"UnitPrice": 32.8,
+		"UnitsInStock": 0,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": true
+	},
+	{
+		"IdProduct": 54,
+		"ProductName": "Tourtière",
+		"IdWarehouse": 6,
+		"UnitPrice": 7.45,
+		"UnitsInStock": 21,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 10,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 55,
+		"ProductName": "Pâté chinois",
+		"IdWarehouse": 6,
+		"UnitPrice": 24.0,
+		"UnitsInStock": 115,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 20,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 56,
+		"ProductName": "Gnocchi di nonna Alice",
+		"IdWarehouse": 5,
+		"UnitPrice": 38.0,
+		"UnitsInStock": 21,
+		"UnitsOnOrder": 10,
+		"ReorderLevel": 30,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 57,
+		"ProductName": "Ravioli Angelo",
+		"IdWarehouse": 5,
+		"UnitPrice": 19.5,
+		"UnitsInStock": 36,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 20,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 58,
+		"ProductName": "Escargots de Bourgogne",
+		"IdWarehouse": 8,
+		"UnitPrice": 13.25,
+		"UnitsInStock": 62,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 20,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 59,
+		"ProductName": "Raclette Courdavault",
+		"IdWarehouse": 4,
+		"UnitPrice": 55.0,
+		"UnitsInStock": 79,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 60,
+		"ProductName": "Camembert Pierrot",
+		"IdWarehouse": 4,
+		"UnitPrice": 34.0,
+		"UnitsInStock": 19,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 61,
+		"ProductName": "Sirop d'érable",
+		"IdWarehouse": 2,
+		"UnitPrice": 28.5,
+		"UnitsInStock": 113,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 25,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 62,
+		"ProductName": "Tarte au sucre",
+		"IdWarehouse": 3,
+		"UnitPrice": 49.3,
+		"UnitsInStock": 17,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 63,
+		"ProductName": "Vegie-spread",
+		"IdWarehouse": 2,
+		"UnitPrice": 43.9,
+		"UnitsInStock": 24,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 5,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 64,
+		"ProductName": "Wimmers gute Semmelknödel",
+		"IdWarehouse": 5,
+		"UnitPrice": 33.25,
+		"UnitsInStock": 22,
+		"UnitsOnOrder": 80,
+		"ReorderLevel": 30,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 65,
+		"ProductName": "Louisiana Fiery Hot Pepper Sauce",
+		"IdWarehouse": 2,
+		"UnitPrice": 21.05,
+		"UnitsInStock": 76,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 66,
+		"ProductName": "Louisiana Hot Spiced Okra",
+		"IdWarehouse": 2,
+		"UnitPrice": 17.0,
+		"UnitsInStock": 4,
+		"UnitsOnOrder": 100,
+		"ReorderLevel": 20,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 67,
+		"ProductName": "Laughing Lumberjack Lager",
+		"IdWarehouse": 1,
+		"UnitPrice": 14.0,
+		"UnitsInStock": 52,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 10,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 68,
+		"ProductName": "Scottish Longbreads",
+		"IdWarehouse": 3,
+		"UnitPrice": 12.5,
+		"UnitsInStock": 6,
+		"UnitsOnOrder": 10,
+		"ReorderLevel": 15,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 69,
+		"ProductName": "Gudbrandsdalsost",
+		"IdWarehouse": 4,
+		"UnitPrice": 36.0,
+		"UnitsInStock": 26,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 15,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 70,
+		"ProductName": "Outback Lager",
+		"IdWarehouse": 1,
+		"UnitPrice": 15.0,
+		"UnitsInStock": 15,
+		"UnitsOnOrder": 10,
+		"ReorderLevel": 30,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 71,
+		"ProductName": "Flotemysost",
+		"IdWarehouse": 4,
+		"UnitPrice": 21.5,
+		"UnitsInStock": 26,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 72,
+		"ProductName": "Mozzarella di Giovanni",
+		"IdWarehouse": 4,
+		"UnitPrice": 34.8,
+		"UnitsInStock": 14,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 0,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 73,
+		"ProductName": "Röd Kaviar",
+		"IdWarehouse": 8,
+		"UnitPrice": 15.0,
+		"UnitsInStock": 101,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 5,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 74,
+		"ProductName": "Longlife Tofu",
+		"IdWarehouse": 7,
+		"UnitPrice": 10.0,
+		"UnitsInStock": 4,
+		"UnitsOnOrder": 20,
+		"ReorderLevel": 5,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 75,
+		"ProductName": "Rhönbräu Klosterbier",
+		"IdWarehouse": 1,
+		"UnitPrice": 7.75,
+		"UnitsInStock": 125,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 25,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 76,
+		"ProductName": "Lakkalikööri",
+		"IdWarehouse": 1,
+		"UnitPrice": 18.0,
+		"UnitsInStock": 57,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 20,
+		"Discontinued": false
+	},
+	{
+		"IdProduct": 77,
+		"ProductName": "Original Frankfurter grüne Soße",
+		"IdWarehouse": 2,
+		"UnitPrice": 13.0,
+		"UnitsInStock": 32,
+		"UnitsOnOrder": 0,
+		"ReorderLevel": 15,
+		"Discontinued": false
+	}
+
+]
